9853e58a2e8fb325462d295d213f1a8e9618bac9
SharkDc <54718872+SharkDc@users.noreply.github.com>
2024-09-13 10:52:49 -0500

    add files

---
+++ script.js
@@ -0,0 +1,24 @@
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 10000);
+  };
+  
+  document.addEventListener('DOMContentLoaded', () => {
+    document.getElementById('downloadBtn').addEventListener('click', () => {
+        // URL del archivo ya creado en el servidor
+        const fileUrl = 'tu-carta.txt';
+
+        // Crea un enlace temporal
+        const a = document.createElement('a');
+        a.href = fileUrl;
+        a.download = 'archivo.txt'; // Nombre del archivo que se descargará
+        document.body.appendChild(a);
+        a.click();
+
+        // Limpia el enlace temporal
+        document.body.removeChild(a);
+    });
+});
+
