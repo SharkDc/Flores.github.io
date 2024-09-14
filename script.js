46ecb84903bc7119717ec97b2ca87447a0a64ca8
DearShark <luistf2202@gmail.com>
2024-09-14 10:01:05 -0500

    test swal

--- script.js
+++ script.js
@@ -1,4 +1,18 @@
 onload = () => {
+
+  Swal.fire({
+    title: '¿Estás seguro?',
+    text: "Esta acción no se puede deshacer",
+    icon: 'warning',
+    showCancelButton: true,
+    confirmButtonColor: '#3085d6',
+    cancelButtonColor: '#d33',
+    confirmButtonText: 'Sí, bórralo'
+  }).then((result) => {
+    if (result.isConfirmed) {
+      Swal.fire('Borrado!', 'Tu archivo ha sido borrado.', 'success')
+    }
+  })
     const c = setTimeout(() => {
       document.body.classList.remove("not-loaded");
       clearTimeout(c);
@@ -10,8 +24,8 @@ onload = () => {
         // URL del archivo ya creado en el servidor
         const fileUrl = 'tu-carta.txt';
 
-          // Ejemplo básico
-          Swal.fire('Hola', 'Este es un mensaje de ejemplo', 'info');
+        // Ejemplo básico
+        Swal.fire('Hola', 'Este es un mensaje de ejemplo', 'info');
         // Crea un enlace temporal
         const a = document.createElement('a');
         a.href = fileUrl;
